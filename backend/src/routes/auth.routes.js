e0b4bd588be65ffe4632b7d6e1fb8a92e91b376b
Pranav-7262 <varpepranav8@gmail.com>
2026-01-29 20:13:39 +0530

    added auth routes.

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -6,7 +6,8 @@ import {
   refreshAccessToken,
   logoutUser,
   getCurrentUser,
-  updateProfile,
+  updateAccountDetails,
+  changeCurrentPassword,
 } from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 const router = express.Router();
@@ -17,5 +18,6 @@ router.post("/refresh", refreshAccessToken); // this route having no auth middle
 router.post("/logout", verifyJWT, logoutUser);
 
 router.get("/current-user", verifyJWT, getCurrentUser);
-router.put("/update-profile", verifyJWT, updateProfile);
+router.put("/update-profile", verifyJWT, updateAccountDetails);
+router.put("/update-password", verifyJWT, changeCurrentPassword);
 export default router;
